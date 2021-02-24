cf109aee3a30ace79a5e5aecd9af8f95d4fc3410
Jordan Jueckstock <jjuecks@ncsu.edu>
2021-02-24 16:03:34 -0500

    specific category grabbing

--- main.js
+++ main.js
@@ -75,6 +75,41 @@ const clonedSeedProfile = async (seedPath) => {
     return profile;
 };
 
+const asyncSleep = (ms) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+};
+
+const launchWithRetry = async (puppeteerArgs, retries, computeTimeout) => {
+    // default to 5 retries with a base-2 exponential-backoff delay between each retry (1s, 2s, 4s, ...)
+    retries = retries || 5;
+    computeTimeout = computeTimeout || (tryIndex => Math.pow(2, tryIndex - 1) * 1000)
+
+    try {
+        return await puppeteer.launch(puppeteerArgs);
+    } catch (err) {
+        console.warn(`Failed to launch browser, ${retries} left;`, err);
+    }
+
+    for (let i = 1; i <= retries; ++i) {
+        await asyncSleep(computeTimeout(i));
+        try {
+            return await puppeteer.launch(puppeteerArgs)
+        } catch (err) {
+            console.warn(`Failed to launch browser, ${retries - i} left;`, err);
+        }
+    }
+
+    throw new Error(`Unable to launch browser after ${retries} retries!`)
+}
+
+const pageMonitor = (page) => {
+    return new Promise((resolve, reject) => {
+        page.on('close', resolve);
+        page.on('error', reject);
+    });
+};
 
 const runSingleTest = async (url, profilePath, tracePath, cliOptions) => {
     const puppeteerArgs = {
@@ -108,86 +143,33 @@ const runSingleTest = async (url, profilePath, tracePath, cliOptions) => {
         puppeteerArgs.args.push(...JSON.parse(cliOptions.args));
     }
     
-    const browser = await puppeteer.launch(puppeteerArgs);
-    const browserPort = (new URL(browser.wsEndpoint())).port;
-    const lhOpts = {
-        logLevel: 'info',
-        output: cliOptions.format || 'html',
-        onlyCategories: ['performance'],
-        port: browserPort,
-    };
-    //const runnerResult = await lighthouse(url, lhOpts);
-    const page = await browser.newPage();
-    await page.tracing.start({
-        path: tracePath,
-        screenshots: false,
-    });
-    const response = page.goto(url, {
-        waitUntil: 'load',
-        // TODO: make nav-timeout a parameter
-    });
-    await Promise.race([
-        response,
-        asyncSleep(60000), // TODO: make me a parameter
-    ]);
-    await page.tracing.stop();
-    await browser.close();
-    return {
-        lhr: [], // dummy Lighthouse report
-        report: "no report dummy!", 
-    };
-};
-
-
-const lookupJPath = (obj, jpath, missingValue) => {
-    const segs = jpath.split('.');
-    for (const seg of segs) {
-        if (!(seg in obj)) {
-            return missingValue;
-        }
-        obj = obj[seg];
+    const browser = await launchWithRetry(puppeteerArgs);
+    try {
+        const page = await browser.newPage();
+        const pageCrashedOrClosed = pageMonitor(page);
+        await page.tracing.start({
+            path: tracePath,
+            screenshots: false,
+            categories: [
+                'devtools.timeline', // for all page-load/request metrics records
+                'disabled-by-default-devtools.timeline', // for useful "FrameCommittedInBrowser" control record (and a ton of other junk...)
+                'disabled-by-default-v8.runtime_stats', 
+            ],
+        });
+        const loadedResponse = page.goto(url, {
+            waitUntil: 'load',
+            timeout: cliOptions.navTimeout * 1000,
+        });
+        const waitingForLoaded = asyncSleep(cliOptions.timeout * 1000);
+        await Promise.race([
+            pageCrashedOrClosed,
+            loadedResponse,
+            waitingForLoaded,
+        ]);
+        await page.tracing.stop();
+    } finally {
+        await browser.close();
     }
-    return obj;
-};
-
-
-// Extract desired elements from a full Lighthouse report JS object
-const cookLighthouseReport = (lhr) => {
-    const globalStatRules = [
-        ['fcp', 'audits.first-contentful-paint.numericValue'],
-        ['lcp', 'audits.largest-contentful-paint.numericValue'],
-        ['tbt', 'audits.total-blocking-time.numericValue'],
-        ['fcidle', 'audits.first-cpu-idle.numericValue'],
-        ['crcmax', 'audits.critical-request-chains.details.longestChain.duration'],
-        ['mwsum', 'audits.mainthread-work-breakdown.numericValue'],
-    ];
-
-    const globalStats = {};
-    for (const [tag, jpath] of globalStatRules) {
-        globalStats[tag] = lookupJPath(lhr, jpath);
-    }
-
-    const mainthreadWorkGroups = ['scriptEvaluation', 'other', 'scriptParseCompile', 'garbageCollection'];
-    const mainthreadWorkItems = lookupJPath(lhr, 'audits.mainthread-work-breakdown.details.items', []);
-    const mainthreadWorkStats = {};
-    for (const {group, duration} of mainthreadWorkItems) {
-        if (mainthreadWorkGroups.includes(group)) {
-            mainthreadWorkStats[group] = duration;
-        }
-    }
-
-    const bootupItems = lookupJPath(lhr, 'audits.bootup-time.details.items');
-    return {
-        global: globalStats,
-        mainthreadWork: mainthreadWorkStats,
-        bootup: bootupItems,
-    };
-};
-
-const asyncSleep = (ms) => {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
 };
 
 const runTestSet = async (url, cliOptions) => {
@@ -203,7 +185,6 @@ const runTestSet = async (url, cliOptions) => {
             raii.push(() => xvfb.stop());
         }
 
-        const aggStats = [];
         let timeLeft = 0;
         for (let i = 0; i < cliOptions.count; ++i) {
             try {
@@ -212,19 +193,8 @@ const runTestSet = async (url, cliOptions) => {
                     await asyncSleep(timeLeft);
                 }
                 const traceFilename = path.join(cliOptions.directory, `trace.${i}.json`);
-                const runnerResult = await runSingleTest(url, profile.path, traceFilename, cliOptions);
+                await runSingleTest(url, profile.path, traceFilename, cliOptions);
                 const testEndedAt = Date.now();
-
-                // `.lhr` is the Lighthouse Result as a JS object
-                const resultStats = cookLighthouseReport(runnerResult.lhr);
-                resultStats.url = url;
-                resultStats.visit = i + 1;
-                aggStats.push(resultStats);
-
-                // `.report` is the HTML/JSON report as a string
-                const reportHtml = runnerResult.report;
-                const reportFilename = path.join(cliOptions.directory, `lhreport.${i}.${cliOptions.format || 'html'}`);
-                await fs.writeFile(reportFilename, reportHtml).catch(err => console.error(err));
 
                 // compute how much longer we should wait to keep our specified inter-test spacing
                 timeLeft = (cliOptions.wait * 1000) - (Date.now() - testEndedAt);
@@ -233,8 +203,6 @@ const runTestSet = async (url, cliOptions) => {
             }
         }
 
-        const aggStatFilename = path.join(cliOptions.directory, 'aggstats.json');
-        await fs.writeFile(aggStatFilename, JSON.stringify(aggStats, undefined, 4));
     } finally {
         await Promise.allSettled(raii.map(x => x()));
     }
@@ -246,14 +214,16 @@ am(async () => {
         .arguments('<url>')
         .option('-a, --args <json_array>', 'pass additional CLI args to Chromium', '[]')
         .option('-b, --binary <path>', 'launch Chromium binary found at <path> instead of system-default')
-        .option('-d, --directory <path>', 'save Lighthouse reports to <path>', '.')
         .option('-c, --count <n>', 'perform <n> repetitions of the test', 1)
+        .option('-d, --directory <path>', 'save Lighthouse reports to <path>', '.')
         .option('-f, --format <name>', 'generate Lighthouse tests in <name> format (html, json)', 'html')
-        .option('-s, --seed <path>', 'use a temp profile cloned from a seed profile at <path>')
-        .option('-x, --xvfb', 'Launch Xvfb automagically')
-        .option('-w, --wait <sec>', 'wait <sec> between test reps to avoid throttling', 1)
-        .option('-v, --verbose', 'turn on verbose stderr logging from Chromium')
+        .option('-n, --navTimeout <sec>', 'abort navigation after <sec>', 30)
         .option('-p, --proxy <url>', 'use <url> as an HTTP/SOCKS proxy server')
+        .option('-s, --seed <path>', 'use a temp profile cloned from a seed profile at <path>')
+        .option('-t, --timeout <sec>', 'wait up to <sec> for page "load" event', 60)
+        .option('-v, --verbose', 'turn on verbose stderr logging from Chromium')
+        .option('-w, --wait <sec>', 'wait <sec> between test reps to avoid throttling', 1)
+        .option('-x, --xvfb', 'Launch Xvfb automagically')
         .action(runTestSet);
     await program.parseAsync();
 });
