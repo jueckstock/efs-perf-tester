91a5d2fa1b8c5d72b5ef8858537a4ae4c941c383
Jordan Jueckstock <jjuecks@ncsu.edu>
2021-03-01 14:20:27 -0500

    wip

---
+++ analysis/harvest-json-stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import sys
+import json
+
+import pandas as pd
+
+
+def main(argv):
+    try:
+        stats_stem = argv[1]
+    except IndexError:
+        print(f"usage: {argv[0]} OUT_CSV_FILE_STEM <STREAM-OF-STATS-JSON-FILENAMES")
+        return
+    
+    def csv_name(tag):
+        return f"{stats_stem}-{tag}.csv"
+    
+    loading_stats = []
+    v8_stats = []
+    req_stats = []
+    for stats_sile in map(str.strip, sys.stdin):
+        with open(stats_sile, 'rb') as fd:
+            raw_stats = json.load(fd)
+        
+        ratio_stats = raw_stats['ratio']
+        loading_stats.append(ratio_stats['loading'])
+        for key, stats in ratio_stats['v8'].items():
+            v8_stats.append(dict(origin=key, **stats))
+        for key, stats in ratio_stats['req'].items():
+            req_stats.append(dict(origin=key, **stats))
+    
+    ldf = pd.DataFrame(loading_stats)
+    ldf.to_csv(csv_name('loading'), index_label=False)
+
+    vdf = pd.DataFrame(v8_stats)
+    vdf.to_csv(csv_name('v8'), index_label=False)
+
+    rdf = pd.DataFrame(req_stats)
+    rdf.to_csv(csv_name('req'), index_label=False)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
--- analysis/ratio-curves.py
+++ analysis/ratio-curves.py
@@ -1,1 +1,45 @@
 #!/usr/bin/env python3
+import sys
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+def main(argv):
+    try:
+        stem_a = argv[1]
+        stem_b = argv[2]
+    except IndexError:
+        print(f"usage: {argv[0]} STATS_STEM_A STATS_STEM_B")
+        return
+    out_stem = f"OUT-{stem_a}-{stem_b}"
+    
+    def csv_a(tag):
+        return f"{stem_a}-{tag}.csv"
+
+    def csv_b(tag):
+        return f"{stem_b}-{tag}.csv"
+    
+    # PLOT LOADING TIMES COMPARISON CURVES
+    #-------------------------------------
+
+    ldf1 = pd.read_csv(csv_a('loading'), index_col=0)
+    ldf2 = pd.read_csv(csv_b('loading'), index_col=0)
+
+    col_pairs = list(zip(ldf1.columns, ldf2.columns))
+    assert all(a == b for a, b in col_pairs), "mismatched columns!"
+
+    fig, axen = plt.subplots(len(col_pairs), 1, figsize=(6, 4 * len(col_pairs)), sharex=True)
+    for i, (a, _) in enumerate(col_pairs):
+        tdf = pd.DataFrame({stem_a: ldf1[a], stem_b: ldf2[a]})
+        tdf.plot.density(ax=axen[i], title=a, xlim=(0.5, 2.0))
+
+    #fig, axen = plt.subplots(1, 2, sharey=True)
+    #ldf1.plot.density(ax=axen[0], title=stem_a)
+    #df2.plot.density(ax=axen[1], title=stem_b)
+    #plt.show()
+    fig.savefig(f"{out_stem}-loading.pdf")
+    
+
+
+if __name__ == "__main__":
+    main(sys.argv)
