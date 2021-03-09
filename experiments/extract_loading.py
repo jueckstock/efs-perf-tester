fb03580e3381313a9ad64e80bf3f31067c8600f5
Jordan Jueckstock <jjuecks@ncsu.edu>
2021-03-09 13:53:52 -0500

    figuring out weird humps in loading stats

--- experiments/extract_loading.py
+++ experiments/extract_loading.py
@@ -18,11 +18,13 @@ def main(argv):
     per_policy = defaultdict(list)
     for i, visit_record in enumerate(stats_dump):
         try:
+            visit_url = visit_record["url"]
             policy_name = visit_record["policy"]
             policy_list = per_policy[policy_name]
             for ckey, cycle in visit_record["visits"].items():
                 try:
                     loading_stats = {label: data["loading"] for label, data in cycle["stats"].items()}
+                    loading_stats["url"] = visit_url
                     policy_list.append(loading_stats)
                 except KeyError as err:
                     print(f"record[{i}], policy[{policy_name}], cycle[{ckey}] malformed, missing key {err}", file=sys.stderr)
@@ -33,15 +35,15 @@ def main(argv):
             json.dump(visit_record, sys.stderr, indent=2)
             print(file=sys.stderr)
     
-
     writer = csv.writer(sys.stdout)
-    writer.writerow(['metric', 'policy', 'temp', 'seconds'])
+    writer.writerow(['url', 'metric', 'policy', 'temp', 'seconds'])
     for policy_name, stats_list in per_policy.items():
         for cycle in stats_list:
+            visit_url = cycle["url"]
             for metric, diff_value in cycle["diff"].items():
                 writer.writerows([
-                    [metric, policy_name, 'cold', cycle["cold"][metric] / 1_000_000],
-                    [metric, policy_name, 'hot', cycle["hot"][metric] / 1_000_000],
+                    [visit_url, metric, policy_name, 'cold', cycle["cold"][metric] / 1_000_000],
+                    [visit_url, metric, policy_name, 'hot', cycle["hot"][metric] / 1_000_000],
                 ])
 
 
