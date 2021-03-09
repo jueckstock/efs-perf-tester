27999ad22d98618a51c674e21afb1e25d7560134
Jordan Jueckstock <jjuecks@ncsu.edu>
2021-03-09 10:56:44 -0500

    req extraction

--- experiments/extract_req.py
+++ experiments/extract_req.py
@@ -31,16 +31,16 @@ def main(argv):
                     labeled_stats = {}
                     for label, data in cycle["stats"].items():
                         labeled_list = {}
-                        for raw_exe_url, stats in data["v8"].items():
+                        for raw_origin_url, stats in data["req"].items():
                             try:
-                                exe_url = urlparse(raw_exe_url)
+                                origin_url = urlparse(raw_origin_url)
                             except ValueError as ux:
-                                print(f"unparseable execution context URL: '{raw_exe_url}': {ux}", file=sys.stderr)
+                                print(f"unparseable execution context URL: '{raw_origin_url}': {ux}", file=sys.stderr)
                                 continue
                             # 3p only!
-                            exe_etld1 = get_sld(exe_url.hostname)
-                            if (exe_etld1 is not None) and (exe_etld1 != visit_etld1):
-                                labeled_list[exe_etld1] = stats
+                            origin_etld1 = get_sld(origin_url.hostname)
+                            if (origin_etld1 is not None) and (origin_etld1 != visit_etld1):
+                                labeled_list[origin_etld1] = stats
                         if labeled_list:
                             labeled_stats[label] = labeled_list
                     if 'diff' in labeled_stats:
@@ -57,13 +57,13 @@ def main(argv):
     #return per_policy
 
     writer = csv.writer(sys.stdout)
-    writer.writerow(['tpetld1', 'policy', 'temp', 'invocations', 'microseconds'])
+    writer.writerow(['tpetld1', 'policy', 'temp', 'requests', 'bytes'])
     for policy_name, stats_list in per_policy.items():
         for cycle in stats_list:
             for tpetld1 in cycle["diff"]:
                 writer.writerows([
-                    [tpetld1, policy_name, 'cold', cycle["cold"][tpetld1]["count"], cycle["cold"][tpetld1]["microseconds"]],
-                    [tpetld1, policy_name, 'hot', cycle["hot"][tpetld1]["count"], cycle["hot"][tpetld1]["microseconds"]],
+                    [tpetld1, policy_name, 'cold', cycle["cold"][tpetld1]["count"], cycle["cold"][tpetld1]["bytes"]],
+                    [tpetld1, policy_name, 'hot', cycle["hot"][tpetld1]["count"], cycle["hot"][tpetld1]["bytes"]],
                 ])
 
 
